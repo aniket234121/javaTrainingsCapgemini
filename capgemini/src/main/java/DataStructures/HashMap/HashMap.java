639811a37bae0534ec6c36a87e7285781378adde
aniket234121 <aniketchauhan329@gmail.com>
2025-04-17 11:52:04 +0530

    April 16 commit

--- capgemini/src/main/java/DataStructures/HashMap/HashMap.java
+++ capgemini/src/main/java/DataStructures/HashMap/HashMap.java
@@ -14,6 +14,8 @@ public class HashMap {
     public int getIndex(Integer key){
         return Math.abs(key.hashCode())%capacity;
     }
+    private final float loadFactor = 0.75f;
+    private int size;
     private int capacity;
     private Node [] Buckets;
     public HashMap(){
@@ -26,12 +28,13 @@ public class HashMap {
     }
     public void put(Integer key,String Value){
         int index=getIndex(key);
-        if(Buckets[index]==null){
-            Buckets[index]=new Node(key,Value);
-            return;
-        }
+//        if(Buckets[index]==null){
+//            Buckets[index]=new Node(key,Value);
+//            size++;
+//            return;
+//        }
         Node head=Buckets[index];
-        while(head.next!=null){
+        while(head!=null){
             if(head.key.equals(key)){
                 head.value=Value;
                 return;
@@ -40,6 +43,10 @@ public class HashMap {
         Node newNode=new Node(key,Value);
         newNode.next=Buckets[index];
         Buckets[index]=newNode;
+        this.size++;
+        if(size>=capacity*loadFactor){
+            reSize();
+        }
     }
     public String get(Integer key){
         int index=getIndex(key);
@@ -51,5 +58,23 @@ public class HashMap {
             head=head.next;
         }
         return null;
+    }
+
+    public void reSize()
+    {
+        System.out.println("resizing");
+        capacity=capacity*2;
+        Node [] oldBuckets=Buckets;
+        Buckets=new Node[capacity];
+        size=0;
+        for(int i=0;i<oldBuckets.length;i++){
+            Node head=oldBuckets[i];
+            while(head!=null)
+            {
+                put(head.key,head.value);
+                head=head.next;
+            }
+        }
+
     }
 }
--- capgemini/src/main/java/DataStructures/HashMap/HashMapDriver.java
+++ capgemini/src/main/java/DataStructures/HashMap/HashMapDriver.java
@@ -2,11 +2,17 @@ package DataStructures.HashMap;
 
 public class HashMapDriver {
     public static void main(String[] args) {
-        HashMap hashMap = new HashMap();
+        HashMap hashMap = new HashMap(10);
         hashMap.put(1,"aniket");
         hashMap.put(2,"ram");
         hashMap.put(3,"helo");
-        hashMap.put(2,"kateru");
+        hashMap.put(6,"kateru");
+
+        hashMap.put(7,"kateru");
+        hashMap.put(4,"asdfa");
+        hashMap.put(5,"asdfasdf");
+        hashMap.put(8,"asdfasdf");
+
         System.out.println(hashMap.get(2));
     }
 }
